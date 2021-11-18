7a4e4601ad2b27844705bb09ffed96e4ffc0cf22
charifamsd <94488467+charifamsd@users.noreply.github.com>
2021-11-18 23:35:27 +0100

    Add files via upload

---
+++ apprentissage1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int main(){
+	printf("charifa zwina\n");
+	int x;
+	scanf ("%d",&x);
+	printf ("x=%d \n",x);
+	float a,b;
+	a=5.2;
+	b=6.9;
+	printf ("a=%f,b=%f \n",a,b);
+	printf ("a+b=%f",a+b);
+	return 0;
+}
